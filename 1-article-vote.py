d90ebb5490c8f71f7875002add8dce1f31efaa7d
adu <aluta182004@gmail.com>
2023-09-19 15:41:18 +0000

    refactored code

--- 1-article-vote.py
+++ 1-article-vote.py
@@ -1,8 +1,13 @@
 import time
+import redis
 
 ONE_WEEK_IN_SECONDS = 7 * 86400
 VOTE_SCORE = 432
 ARTICLES_PER_PAGE = 25
+SCORE_KEY = "score:"
+ARTICLE_KEY = "article:"
+VOTE_KEY= "voted:"
+GROUP_KEY="group:"
 
 def article_vote(conn, user, article):
     cut_off = time.Time() - ONE_WEEK_IN_SECONDS
@@ -10,19 +15,19 @@ def article_vote(conn, user, article):
         return
     
     article_id = article.partition(':')[-1]
-    if conn.sadd('voted:'+article_id, user):
-        conn.zincrby('score:', article, VOTE_SCORE)
+    if conn.sadd(VOTE_KEY + article_id, user):
+        conn.zincrby(SCORE_KEY, article, VOTE_SCORE)
         conn.hincrby(article,'votes', 1)
         
 
 def post_article(conn, user, title, link):
-    article_id = str(conn.incr('article:'))
-    voted = 'voted:' + article_id
+    article_id = str(conn.incr(ARTICLE_KEY))
+    voted = VOTE_KEY + article_id
     conn.sadd(voted, user)
     conn.expire(voted, ONE_WEEK_IN_SECONDS)
     
     now = time.time()
-    article = 'article:'+article_id
+    article = ARTICLE_KEY+article_id
     conn.hmset(article,{
         'title':title,
         'link':link,
@@ -31,7 +36,7 @@ def post_article(conn, user, title, link):
         'votes': 1
     })        
     
-    conn.zadd('score:', article, now + VOTE_SCORE)
+    conn.zadd(ARTICLE_KEY, article, now + VOTE_SCORE)
     conn.zadd('time:', article, now)
     
     return article_id
@@ -52,17 +57,20 @@ def get_articles(conn, page, order='score:'):
 
 
 def add_remove_groups(conn, artcile_id, to_add=[], to_remove=[]):
-    article = 'article:'+artcile_id
+    article = ARTICLE_KEY+artcile_id
     for group in to_add:
-        conn.sadd('group:'+group, article)
+        conn.sadd(GROUP_KEY+group, article)
     for group in to_remove:
-        conn.srem('group:'+group,article)  
+        conn.srem(GROUP_KEY+group,article)  
         
 
 def get_group_articles(conn, group, page, order='score:'):
     key = order + group
     if not conn.exists(key):
-        conn.zinterstore(key,['group:'+group,order],aggregate='max')
+        conn.zinterstore(key,[GROUP_KEY+group,order],aggregate='max')
         conn.expire(key, 60)
     
-    return get_articles(conn, page, key)                 
+    return get_articles(conn, page, key) 
+
+
+conn = redis.Redis()                
